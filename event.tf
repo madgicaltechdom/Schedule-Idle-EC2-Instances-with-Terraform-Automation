8bdb686825623487e3fc3d6b03eebf98df1f7dda
kaumudi766 <109335469+kaumudi766@users.noreply.github.com>
2023-01-25 12:21:06 +0530

    Update event.tf

--- event.tf
+++ event.tf
@@ -4,7 +4,6 @@ resource "aws_cloudwatch_event_rule" "EC2_stop" {
   description         = "Stops EC2 instance on a schedule"
   schedule_expression = "cron(${var.cron_stop})"
 }
-
 resource "aws_cloudwatch_event_target" "EC2_stop" {
   count = var.enable ? 1 : 0
   arn   = "arn:aws:ssm:${data.aws_region.current.name}::automation-definition/AWS-StopEC2Instance:$DEFAULT"
@@ -14,21 +13,18 @@ resource "aws_cloudwatch_event_target" "EC2_stop" {
         aws_iam_role.ssm_automation[0].arn,
       ]
       InstanceId = local.source_instances
-
     }
   )
   role_arn  = aws_iam_role.event[0].arn
   rule      = aws_cloudwatch_event_rule.EC2_stop[0].name
   target_id = "EC2-scheduler-${local.identifier}-stop"
 }
-
 resource "aws_cloudwatch_event_rule" "EC2_start" {
   count               = var.enable ? 1 : 0
   name                = "EC2-scheduler-${local.identifier}-start"
   description         = "Starts EC2 instance on a schedule"
   schedule_expression = "cron(${var.cron_start})"
 }
-
 resource "aws_cloudwatch_event_target" "EC2_start" {
   count = var.enable ? 1 : 0
   arn   = "arn:aws:ssm:${data.aws_region.current.name}::automation-definition/AWS-StartEC2Instance:$DEFAULT"
@@ -38,7 +34,6 @@ resource "aws_cloudwatch_event_target" "EC2_start" {
         aws_iam_role.ssm_automation[0].arn,
       ]
       InstanceId = local.source_instances
-
     }
   )
   role_arn  = aws_iam_role.event[0].arn
