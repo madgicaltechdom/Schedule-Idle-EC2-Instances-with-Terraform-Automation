a27e0f294569261fe0e79c3e3e0f9b3558f9753f
kaumudi766 <109335469+kaumudi766@users.noreply.github.com>
2023-01-25 12:33:02 +0530

    Update event.tf

--- event.tf
+++ event.tf
@@ -4,6 +4,7 @@ resource "aws_cloudwatch_event_rule" "EC2_stop" {
   description         = "Stops EC2 instance on a schedule"
   schedule_expression = "cron(${var.cron_stop})"
 }
+
 resource "aws_cloudwatch_event_target" "EC2_stop" {
   count = var.enable ? 1 : 0
   arn   = "arn:aws:ssm:${data.aws_region.current.name}::automation-definition/AWS-StopEC2Instance:$DEFAULT"
@@ -19,12 +20,14 @@ resource "aws_cloudwatch_event_target" "EC2_stop" {
   rule      = aws_cloudwatch_event_rule.EC2_stop[0].name
   target_id = "EC2-scheduler-${local.identifier}-stop"
 }
+
 resource "aws_cloudwatch_event_rule" "EC2_start" {
   count               = var.enable ? 1 : 0
   name                = "EC2-scheduler-${local.identifier}-start"
   description         = "Starts EC2 instance on a schedule"
   schedule_expression = "cron(${var.cron_start})"
 }
+
 resource "aws_cloudwatch_event_target" "EC2_start" {
   count = var.enable ? 1 : 0
   arn   = "arn:aws:ssm:${data.aws_region.current.name}::automation-definition/AWS-StartEC2Instance:$DEFAULT"
@@ -40,8 +43,3 @@ resource "aws_cloudwatch_event_target" "EC2_start" {
   rule      = aws_cloudwatch_event_rule.EC2_start[0].name
   target_id = "EC2-scheduler-${local.identifier}-start"
 }
-
-
-
-
-
