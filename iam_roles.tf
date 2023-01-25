607e353f3a45bf815cb565284fe7071c10c5fa18
kaumudi766 <109335469+kaumudi766@users.noreply.github.com>
2023-01-25 12:39:04 +0530

    Update iam_roles.tf

--- iam_roles.tf
+++ iam_roles.tf
@@ -16,7 +16,6 @@ data "aws_iam_policy_document" "event_trust" {
   count = var.enable ? 1 : 0
   statement {
     actions = ["sts:AssumeRole"]
-
     principals {
       type        = "Service"
       identifiers = ["events.amazonaws.com"]
@@ -27,29 +26,23 @@ data "aws_iam_policy_document" "event_trust" {
 # Generate a random string to add it to the name of the Target Group
 resource "random_string" "iam_suffix" {
   length = 12
-
   min_numeric = 12
 }
-
 resource "aws_iam_role" "event" {
   count              = var.enable ? 1 : 0
   name               = substr("EC2-scheduler-${local.identifier}-${random_string.iam_suffix.result}", 0, 64)
   assume_role_policy = data.aws_iam_policy_document.event_trust[0].json
 }
-
 resource "aws_iam_role_policy" "event" {
   count  = var.enable ? 1 : 0
   name   = substr("EC2-scheduler-${local.identifier}-${random_string.iam_suffix.result}", 0, 64)
   policy = data.aws_iam_policy_document.event[0].json
   role   = aws_iam_role.event[0].name
 }
-
-
 data "aws_iam_policy_document" "ssm_automation_trust" {
   count = var.enable ? 1 : 0
   statement {
     actions = ["sts:AssumeRole"]
-
     principals {
       type        = "Service"
       identifiers = ["ssm.amazonaws.com"]
@@ -62,7 +55,6 @@ resource "aws_iam_role" "ssm_automation" {
   name               = substr("EC2-scheduler-ssm-${local.identifier}-${random_string.iam_suffix.result}", 0, 64)
   assume_role_policy = data.aws_iam_policy_document.ssm_automation_trust[0].json
 }
-
 resource "aws_iam_role_policy" "ssm_automation" {
   count  = var.enable ? 1 : 0
   name   = substr("EC2-scheduler-ssm-${local.identifier}-${random_string.iam_suffix.result}", 0, 64)
@@ -85,7 +77,6 @@ resource "aws_iam_role_policy" "ssm_automation" {
   ]
 }
 EOF
-
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_automation" {
